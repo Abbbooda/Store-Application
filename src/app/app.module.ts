48efe5fbee92a53c2756c12ddeeadb75b99f423b
Abbbooda <elkhaloufiabderrahman@gmail.com>
2023-12-31 01:11:59 +0100

    create cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { CategoriesComponent } from './products/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    CartComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
